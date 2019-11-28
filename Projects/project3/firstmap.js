5be030b6141c538d834cd50395d0c91fdc7060d8
Hasani Valdez <54682559+Hvaldez2@users.noreply.github.com>
2019-11-27 18:31:49 -0600

    fixing javascript file

--- Projects/project3/firstmap.js
+++ Projects/project3/firstmap.js
@@ -272,3 +272,31 @@ function initMap() {
 
 }
 //google.maps.event.addDomListener(window, 'load', init);
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2400); // Change image every 2 seconds
+}
+
+showSlides();
